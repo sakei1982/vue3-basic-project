bf0b3b32683cba939852d1fc58d68b76a20cfee1
sakei <29232771@qq.com>
2024-01-03 21:07:56 +0900

    listanddelete

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
+import { ref,onMounted } from 'vue'
 import Edit from './components/Edit.vue'
+import axios from 'axios'
 
 // TODO: 列表渲染
+const list = ref([])
 
+const getList = async () => {
+  const res = await axios.get('/list')
+  list.value=res.data
+}
 
+onMounted(() =>  getList())
 // TODO: 删除功能
-
+const onDelete = async (id) => {
+  await axios.delete(`/del/${id}`)
+  getList()
+}
 
 // TODO: 编辑功能
 
@@ -13,18 +24,14 @@ import Edit from './components/Edit.vue'
 
 <template>
   <div class="app">
-    <el-table :data="[{
-      id: 1,
-      name: 'jack',
-      place: 'none'
-    }]">
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
+        <template #default="{row}">
           <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+          <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
